9ffb3941556e8c2dd02305961d2238e8638fd47c
elenaludina0573 <elenaludina0@gmail.com>
2024-03-09 18:53:41 +0300

    List of companies and number of vacancies

---
+++ Database/db_manager.py
@@ -0,0 +1,17 @@
+import psycopg2
+from Database.getting_data_hh import params_db
+
+
+class DBManager:
+
+    """Класс для работы с информацией из Базы Данных"""
+
+    @staticmethod
+    def get_companies_and_vacancies_count():
+        """Получает список всех компаний и количество вакансий у каждой компании."""
+        with psycopg2.connect(dbname='HH_vacancy', **params_db) as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT company_name, COUNT(vacancy_name) from vacancies GROUP BY company_name')
+                answer = cur.fetchall()
+        conn.close()
+        return answer
