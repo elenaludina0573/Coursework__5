120f618600b1da6bdc855c853267e90d82bfe5d2
elenaludina0573 <elenaludina0@gmail.com>
2024-03-10 22:47:25 +0300

    The final result

---
+++ main.py
@@ -0,0 +1,16 @@
+from Database.getting_data_hh import HH_api_db
+from config import config
+from Database.utils import create_database, create_table
+
+
+def main():
+    params_db = config()
+    create_database('postgres', params_db)
+    create_table(params_db)
+    db_vacancies = HH_api_db()
+    db_vacancies.employers_to_db()
+    db_vacancies.vacancies_to_db()
+
+
+if __name__ == '__main__':
+    main()
