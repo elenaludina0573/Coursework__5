a3a671f2e1cafde69501dc1dfc370224e4ee7835
elenaludina0573 <elenaludina0@gmail.com>
2024-03-15 12:54:34 +0300

    The required file_2

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 from configparser import ConfigParser
+import os
+
+path_os = os.path.dirname(os.path.abspath(__file__))
+file_name = os.path.join(path_os, 'database.ini')
 
 
-def config(filename="database.ini", section="postgresql"):
+def config(filename=file_name, section="postgresql"):
     # create a parser
     parser = ConfigParser()
     # read config file
-    parser.read(filename)
+    parser.read(filename, encoding='utf-8')
     db = {}
     if parser.has_section(section):
         params = parser.items(section)
@@ -15,3 +19,4 @@ def config(filename="database.ini", section="postgresql"):
         raise Exception(
             'Section {0} is not found in the {1} file.'.format(section, filename))
     return db
+
