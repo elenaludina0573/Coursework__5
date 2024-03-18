8619dee8da38426cb0452dd6dbb1d0254d705f73
elenaludina0573 <elenaludina0@gmail.com>
2024-03-18 17:24:06 +0300

    The required file_3

--- config.py
+++ config.py
@@ -9,14 +9,11 @@ def config(filename=file_name, section="postgresql"):
     # create a parser
     parser = ConfigParser()
     # read config file
-    parser.read(filename, encoding='utf-8')
-    db = {}
+    parser.read(filename)
+
     if parser.has_section(section):
         params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
+        db = dict(params)
     else:
-        raise Exception(
-            'Section {0} is not found in the {1} file.'.format(section, filename))
+        raise Exception("Section {0} is not found in the {1} file".format(section, filename))
     return db
-
